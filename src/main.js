e4294222c811ba87e6bda87f8358cc2d20f567e6
khuong <khuongtunha1156@gmail.com>
2024-10-31 19:27:32 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import './assets/main.css'
+import { createApp } from "vue"; 
+import App from "./App.vue";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import './assets/main.css';
 
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import router from "./router";
+
+createApp(App).use(router).mount("#app")
 createApp(App).mount('#app')
